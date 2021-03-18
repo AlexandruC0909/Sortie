212ed207f1e8943758e51b708153b51b113e14a4
Maxime-IN <77492401+Maxime-IN@users.noreply.github.com>
2021-03-18 09:21:24 +0100

    Ajout SearchParticipantsType

---
+++ src/Form/SearchParticipantType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo', SearchType::class,
+            [
+                'label' => 'Rechercher par Pseudo',
+                'attr' => [
+                    'placeholder' => 'Pseudo de l\'utilisateur',
+                ],
+                'required' => false
+            ])
+
+            ->add('Rechercher', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
